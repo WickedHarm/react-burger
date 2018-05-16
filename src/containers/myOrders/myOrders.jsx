657002f9c5904403f5b903a42a3fa12c99c242ab
WickedHarm <littleharm2008@gmail.com>
2018-05-16 22:30:02 +0300

    delete_all_orders

--- src/containers/myOrders/myOrders.jsx
+++ src/containers/myOrders/myOrders.jsx
@@ -5,10 +5,25 @@ import Order from "./Order/Order";
 import Spinner from "../../components/UI/modal/Spinner/Spinner";
 import Error from "../../components/UI/modal/Error";
 
+import classes from "./myOrders.css";
+
 class myOrders extends Component {
     state = {
         loading: false,
         orders: []
+    }
+
+    clearAll = () => {
+        
+        axiosOrder.delete("/orders.json")
+            .then( res => {
+                this.setState({
+                    orders: []
+                })
+            })
+            .catch( e => console.log(e))
+        
+        
     }
 
     getOrders() {
@@ -51,14 +66,11 @@ class myOrders extends Component {
     render() {
         
         return(
-            <div style={{textAlign: "center"}}>
-                
+            <div className={classes.MyOrders}>
+                <span className={classes.DeleteBtn} onClick={this.clearAll}>DELETE ALL ORDERS</span>
                 {this.state.orders.length === 0 ? <h2>You have no orders yet</h2> : null}
                 {this.state.loading ? <Spinner /> : null}
-                {this.state.orders.map( (order) => {
-                console.log(order)
-                    return <Order deleteHandler={this.deleteHandler} key={order.id} order={order}/>
-                } )}
+                {this.state.orders.map( (order) => <Order deleteHandler={this.deleteHandler} key={order.id} order={order}/>)}
             </div>
         );
     }
